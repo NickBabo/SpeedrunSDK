abd27870751c491bdfc561553d2e7c64070b0757
Nicholas Babo <nickbabo7@gmail.com>
2021-03-21 17:43:26 -0300

    Add base SDK protocol

---
+++ SpeedrunSDK/SpeedrunSDK.h
@@ -0,0 +1,18 @@
+//
+//  SpeedrunSDK.h
+//  SpeedrunSDK
+//
+//  Created by Nick Babo on 21/03/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SpeedrunSDK.
+FOUNDATION_EXPORT double SpeedrunSDKVersionNumber;
+
+//! Project version string for SpeedrunSDK.
+FOUNDATION_EXPORT const unsigned char SpeedrunSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpeedrunSDK/PublicHeader.h>
+
+
